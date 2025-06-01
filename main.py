6a4e1e962952a7175a25f380812e26bd2c54f33e
Abdullah Islam <abdullahislam900@gmail.com>
2025-06-02 01:34:39 +0400

    Setting Up Main Game Loop and Menu Options

---
+++ main.py
@@ -0,0 +1,19 @@
+#1st April - I Will Focus on Creating the Starting Sequence for the Game 
+"""
+In This Sequence I will provide a CURRENT eVENTS Time Line 
+Make the Main Game Loop
+Add features for saving, starting, loading save files with a menu 
+"""
+def menu():
+    print("Start New Game - 1")
+    print("Load Save File - 2")
+    print("Game Settings  - 3")
+    print("Quit Game      - 4")
+
+print("The Uber's Sword")
+menu()
+game_state = input("What Would You Like To Do - ")
+
+while game_state != 4: #main game loop
+    pass
+
